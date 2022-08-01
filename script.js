5ad0c18cb9fd596fcf6d2943e344a4e8c5b208b1
Christian Noa <christiannoa20@gmail.com>
2022-08-01 15:31:59 -0400

    first Commit Countfown Timer

---
+++ script.js
@@ -0,0 +1,35 @@
+const daysEl = document.getElementById('days')
+const hoursEl = document.getElementById('hours')
+const minsEl = document.getElementById('mins')
+const secondsEl = document.getElementById('seconds')
+
+
+const release = "29 Oct 2022";
+
+function countdown() {
+    const releaseDate = new Date(release);
+    const currentDate = new Date();
+
+    const totalSeconds = (releaseDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = (Math.floor(totalSeconds / 60) % 60);
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(minutes);
+    secondsEl.innerHTML = formatTime(seconds);
+
+   //console.log(days, hours, minutes, seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+//initial call
+countdown();
+
+setInterval(countdown, 1000);
